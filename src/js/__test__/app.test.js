ee79ec2ea21dbe4003763cbd5fb32f652d0eff34
Pavel Fokin <fokinpavel2007@outlook.com>
2021-10-07 23:00:29 +0300

    first commit

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,17 @@
+﻿import Validator from '../app.js';
+
+test('All rules are matched', () => {
+  expect(Validator.validateUsername('aaa11_-31Zaaaa')).toBe(true);
+});
+
+test('3 numbers or more', () => {
+  expect(Validator.validateUsername('aaa1131Zaaa')).toBe(false);
+});
+
+test('Starts from a number', () => {
+  expect(Validator.validateUsername('1aaa1131aaaa')).toBe(false);
+});
+
+test('Ends with a _', () => {
+  expect(Validator.validateUsername('aaa1131aaaa_')).toBe(false);
+});
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+﻿  
+export default class Validator {
+    static validateUsername(nick) {
+      return /^[a-z][\w-]+[a-z]$/i.test(nick) && !/\d{3,}/.test(nick);
+    }
+  }
